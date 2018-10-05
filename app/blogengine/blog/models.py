9b71f89e9bb17a63852bce8c957a179f5d8aab6e
intuit07 <intuit07@gmail.com>
2018-10-06 01:21:27 +0300

    Add model Post, create requirements.txt

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slag = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
